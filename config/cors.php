ac9ad4dd34cf369156982e8df5e117240e9e3841
agusL1660 <lelliag77@gmail.com>
2024-12-11 20:07:02 -0300

    Actualizo cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:3000','https://motomami-hvtk-agusl1660-agusl1660s-projects.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
